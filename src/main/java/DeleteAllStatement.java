067d221e0ac6cdbb640e3155000d60707c2f8403
hachi <eojin312@naver.com>
2020-04-13 17:43:22 +0900

    수동 DI 전략패턴

--- src/main/java/DeleteAllStatement.java
+++ src/main/java/DeleteAllStatement.java
@@ -21,10 +21,37 @@ public class DeleteAllStatement implements StatementStrategy {
         try {
             DriverManager dataSource = null;
 
-            c = dataSource.getConnection();
+            c = dataSource.getConnection("url");
             StatementStrategy strategy = new DeleteAllStatement();
+            jdbcContextWithStatementStrategy(strategy); //컨텍스트 호출 전략 오브젝트 전달
             ps = strategy.makePreparedStatement(c);
         } catch (Exception e) {
+        }
+    }
+
+    /**
+     * StatementStrategy 타입의 전략을 오브젝트를 제공받고 JDBC 구조로 만들어진 컨텍스트 내에서 작업
+     * 중요한 건 PreparedStatement 가 필요한 시점에 호출해서 사용
+     * @param stmt
+     * @throws SQLException
+     */
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{ //클라이언트가 컨텍스트로 호출할 때 넘겨줄 전략 파라미터
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            DataSource dataSource = null;
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (c != null) {
+                c.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
         }
     }
 }
