21363dc05b228d3ebde063ee8e4b956523f9d189
hachi <eojin312@naver.com>
2020-04-21 18:04:06 +0900

    enum 예제 생성자 생성

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,9 +3,43 @@ public class User {
     private static final int SILVER = 2;
     private static final int GOLD = 3;
 
-    int level;
+    Level level;
+    int login;
+    int recommend;
+    String id;
+    String pwd;
+    String name;
 
-    public void setLevel(int level) {
+    public User(Level level, int login, int recommend, String id, String pwd, String name) {
         this.level = level;
+        this.login = login;
+        this.recommend = recommend;
+        this.id = id;
+        this.pwd = pwd;
+        this.name = name;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
     }
 }
